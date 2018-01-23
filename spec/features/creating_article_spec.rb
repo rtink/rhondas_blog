90b6d02247429ceed22106db89ad6552dffced12
Rhonda Tinkham <rtinkham@live.com>
2018-01-23 01:09:32 +0000

    Article validations and flash

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -1,13 +1,14 @@
 require "rails_helper"
 
 RSpec.feature "Creating Articles" do
-  scenario "A user creates a new article" do
+  scenario "A user fails to create a new article" do
     visit "/"
     click_link "New Article"
-    fill_in "Title", with: "Creating a blog"
-    fill_in "Body", with: "Lorem Ipsum"
+    fill_in "Title", with: ""
+    fill_in "Body", with: ""
     click_button "Create Article"
-    expect(page).to have_content("Article has been created")
-    expect(page.current_path).to eq(articles_path)
+    expect(page).to have_content("Article has not been created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
   end
 end
